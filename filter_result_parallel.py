8eff26470b7043670e33f91e1c04fa1c68ff70fd
gemsanyu <git.gemilang.santiyuda@gmail.com>
2023-01-23 14:05:42 +0700

    cleaned brkga

--- filter_result_parallel.py
+++ filter_result_parallel.py
@@ -249,5 +249,5 @@ dataset_name_list = [
 num_target_solutions=100
 if __name__ == "__main__":
     filter_result_args = [(title, dataset_name, num_target_solutions) for dataset_name in dataset_name_list]
-    with mp.Pool(processes=10) as pool:
+    with mp.Pool(processes=32) as pool:
         L = pool.starmap(filter_result, filter_result_args)
